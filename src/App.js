4090f84660ce0158d47fb7a0f085956fe74a7aaf
Victor Onyekwere <cydollarsign1@gmail.com>
2024-06-21 23:17:57 +0100

    NavBar Created MUI Library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import NavBar from "./components/NavBar"
+import SliderSelect from "./components/SliderSelect"
+import TenureSelect from "./components/TenureSelect"
+import Results from "./components/Results"
 
 function App() {
   return (
     <div>
-      React App
+      <NavBar />
+      <SliderSelect />
+      <TenureSelect />
+      <Results />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Toolbar from '@mui/material/Toolbar'
+
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static' >
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            ShooterCrow
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box >
   )
 }
 
